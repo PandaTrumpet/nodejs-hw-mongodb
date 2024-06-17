e5da919409af065c01c5e7e65c14a894fda27091
PandaTrumpet <dsavontrumpet@gmail.com>
2024-06-17 19:29:07 +0300

    update

--- src/server.js
+++ src/server.js
@@ -3,11 +3,13 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { env } from './utils/env.js';
+import { getAllContacts, getContactById } from './services/contact.js';
 dotenv.config();
 const PORT = Number(env('PORT', '3000'));
 
 export const setupServer = () => {
   const app = express();
+  app.use(express.json());
   app.use(cors());
   app.use(
     pino({
@@ -18,7 +20,27 @@ export const setupServer = () => {
   );
   app.get('/', (req, res, next) => {
     res.json({
-      message: 'Hello',
+      message: 'Hello world',
+    });
+  });
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+    res.status(200).json({
+      data: contacts,
+    });
+  });
+
+  app.get('/contacts/:contactId', async (req, res) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+    if (!contact) {
+      res.status(404).json({
+        message: 'Not found',
+      });
+    }
+    res.status(200).json({
+      data: contact,
+      message: `Successfully found contact with id ${contactId}!`,
     });
   });
   app.use((req, res, next) => {
