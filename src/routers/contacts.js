fc8bdfc3db23c28bf5b2a9c525ef2859acb8df0d
PandaTrumpet <dsavontrumpet@gmail.com>
2024-06-25 22:46:56 +0300

    update

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,19 +1,32 @@
 import { Router } from 'express';
-import { getAllContacts, getContactById } from '../services/contact.js';
+// import { getAllContacts, getContactById } from '../services/contact.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import {
   addContactController,
+  deleteContactController,
   getAllContactsController,
   getContactByIdController,
+  patchContactController,
 } from '../controllers/contacts.js';
-import isValidId from '../middleware/isValidId.js';
+
+// import isValidId from '../middleware/isValidId.js';
 const contactRouter = Router();
 contactRouter.get('/contacts', ctrlWrapper(getAllContactsController));
 
 contactRouter.get(
   '/contacts/:contactId',
-  isValidId,
+
   ctrlWrapper(getContactByIdController),
 );
 contactRouter.post('/contacts', ctrlWrapper(addContactController));
+contactRouter.patch(
+  '/contacts/:contactId',
+
+  ctrlWrapper(patchContactController),
+);
+contactRouter.delete(
+  '/contacts/:contactId',
+
+  ctrlWrapper(deleteContactController),
+);
 export default contactRouter;
