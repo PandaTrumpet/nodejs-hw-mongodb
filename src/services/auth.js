e4f5d063bba5871b0388572ac53f380217e41b8e
PandaTrumpet <dsavontrumpet@gmail.com>
2024-07-21 17:17:56 +0300

    update

--- src/services/auth.js
+++ src/services/auth.js
@@ -27,7 +27,7 @@ export const requestResetToken = async (email) => {
     },
     env('JWT_SECRET'),
     {
-      expiresIn: '15m',
+      expiresIn: '5m',
     },
   );
 
@@ -37,4 +37,11 @@ export const requestResetToken = async (email) => {
     subject: 'Reset your password',
     html: `<p>Click <a href="${resetToken}">here</a> to reset your password!</p>`,
   });
+};
+
+export const resetPassword = async (payload) => {
+  let entries;
+  try {
+    entries = jwt.verify(payload.token, env('JWT_SECRET'));
+  } catch (error) {}
 };
